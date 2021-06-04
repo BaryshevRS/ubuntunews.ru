a3c5e6b852ca5d444f425e92df63061b2d73cc49
baryshevrs <baryshevrs@yandex.ru>
2021-06-05 02:30:51 +0300

    Added header style

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,9 @@ import { getSortedPostsData } from "../lib/posts";
 import LogoIcon from '../public/logo.svg';
 
 import DownloadIcon from '../public/assets/icons/download.svg';
-// import MenuIcon from '../public/Menu.svg';
+import MenuIcon from '../public/assets/icons/menu.svg';
 // import LogoIcon from '../public/Logo.svg';
-// import TimeIcon from '../public/time.svg';
+import TimeIcon from '../public/assets/icons/time.svg';
 
 import FacebookIcon from '../public/assets/icons/facebook.svg';
 import GithubIcon from '../public/assets/icons/github.svg';
@@ -19,9 +19,9 @@ import RssIcon from '../public/assets/icons/rss.svg';
 export default function Home({allPostsData}: any) {
     console.log('allPostsData', allPostsData);
     return (
-        <section className='container'>
+        <div className='wrapper'>
             <Head>
-                <title>Create Next App</title>
+                <title>Новости Ubuntu</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
@@ -33,40 +33,43 @@ export default function Home({allPostsData}: any) {
                 <meta name="msapplication-config" content="favicon/browserconfig.xml"/>
                 <meta name="theme-color" content="#ffffff"/>
             </Head>
-            <header>
+            <header className={'header'}>
                 <div className="logo">
-                    <LogoIcon />
+                    <a href="#"><LogoIcon/></a>
                 </div>
-                <nav>
-                    <ul>
+                <nav className={'nav'}>
+                    <ul className={'nav__menu desktop'}>
                         <li><a href="#">Новости</a></li>
                         <li><a href="#">Статьи</a></li>
                         <li><a href="#">Программы</a></li>
                         <li><a href="#">Об Ubuntu</a></li>
                     </ul>
-
-                    <div className={'download-section'}>
-                        <DownloadIcon />
+                    <div className={'nav__download desktop'}>
+                        <DownloadIcon/>
                         <a href="#">Скачать Ubuntu</a>
                     </div>
-
+                    <div className={'nav__toggle mobile'}><MenuIcon /></div>
                 </nav>
             </header>
-            <main>
+            <main className={'main'}>
+                <h1>Новости Ubuntu</h1>
                 {[1, 2, 3, 4, 5].map((item) => (
-                        <article key={item}>
-                            <figure>
-                                <img width={300}
-                                     src="/img/2014/01/08/22-00/ubuntu14-04-wallpaper-by-mrubuntux-d70iae2-11841092374-o.jpg"
+                        <article className={'post-preview'} key={item}>
+                            <a href="#">
+                            <img width={270} height={165}
+                                 src="/img/2014/01/08/22-00/ubuntu14-04-wallpaper-by-mrubuntux-d70iae2-11841092374-o.jpg"
                                      alt=""/>
-                            </figure>
-                            <header>
-                                <h2>Объявлены первые производители смартфонов c Ubuntu</h2>
-                            </header>
+                            </a>
+                            <a href="#">
+                            <h2>Объявлены первые производители смартфонов c Ubuntu</h2>
+                            </a>
                             <p>Как Марк Шаттлворт и обещал, были объявлены первые производители смартфонов на базе
                                 операционной системы Ubuntu. Ими стали сразу две компании: испанская Bq и китайская
                                 Meizu.</p>
-                            <footer> <time dateTime="2009-09-04T16:31:24+02:00">21.02.2014</time> </footer>
+
+                            <footer>
+                                <TimeIcon /><time dateTime="2009-09-04T16:31:24+02:00">21.02.2014</time>
+                            </footer>
                         </article>
                     )
                 )}
@@ -75,10 +78,9 @@ export default function Home({allPostsData}: any) {
                     <Link href={`/2`}><a href="#">Туда</a></Link>
                     <Link href={`/2`}><a href="#">Сюда</a></Link>
                 </div>
-
             </main>
-            <aside>
-                <section className={'best-section'}>
+            <aside className={'sidebar'}>
+                <section className={'best'}>
                     <h4>Популярное</h4>
 
                     <ul>
@@ -97,48 +99,53 @@ export default function Home({allPostsData}: any) {
                     </ul>
                 </section>
 
+                <footer className={'footer'}>
 
+                    <div className={'footer__nav'}>
+                        <a href="#">О проекте</a>
+                        <a href="#">Сотрудничество</a>
+                    </div>
+
+                    <ul className={'footer__social'}>
+                        <li>
+                            <a href="#">
+                                <FacebookIcon/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <GithubIcon/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <TwitterIcon/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <VkIcon/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <YoutubeIcon/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <RssIcon/>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div className={'footer__copyright'}>
+                        2008–2021 Новости Ubuntu
+                    </div>
+                </footer>
             </aside>
-            <footer>
-                <a href="#">О проекте</a>
-                <a href="#">Сотрудничество</a>
 
-                <ul>
-                    <li>
-                        <a href="#">
-                            <FacebookIcon />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <GithubIcon />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <TwitterIcon />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <VkIcon />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <YoutubeIcon />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <RssIcon />
-                        </a>
-                    </li>
-                </ul>
-
-                2008–2021 Новости Ubuntu
-            </footer>
-        </section>
+        </div>
     )
 }
 
