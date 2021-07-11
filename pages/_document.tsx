ad077f1394eb316b0372d5a1337fa40e8ed4110a
baryshevrs <baryshevrs@yandex.ru>
2021-07-11 12:54:30 +0300

    Added counter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,43 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ru">
-        <Head/>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-17349676-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-17349676-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(2978071, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+          });
+          `,
+            }}
+          />
+          <noscript><div><img src="https://mc.yandex.ru/watch/2978071"
+                              style={{position: 'absolute', left: '-9999px'}} alt="" /></div></noscript>
+        </Head>
         <body>
         <Main/>
         <NextScript/>
