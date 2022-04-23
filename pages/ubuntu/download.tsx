132c553fe5d765386ee8866f6fd7532c6ded920e
BaryshevRS <baryshevrs@yandex.ru>
2022-04-23 15:53:55 +0300

    Text for download 22.04 updated

--- pages/ubuntu/download.tsx
+++ pages/ubuntu/download.tsx
@@ -28,14 +28,13 @@ export default function DownloadPage({topPosts}: ILayoutProps) {
 
         <p>
           Если желаете комфортную рабочую среду с принципом — раз поставил и забыл, то эти версии дистрибутива будут правильным выбором.
-          Так как их поддержка составляет пять лет и по истечению будет продолжительный период с
-          выпуском патчей обновлений безопасности.
+          Так как их поддержка составляет пять лет и по истечению ещё пятилетний период с выпуском патчей обновлений безопасности.
         </p>
 
-        <h2 id="20.04">Скачать Ubuntu 20.04.3 LTS</h2>
+        <h2 id="22.04">Скачать Ubuntu 22.04 LTS</h2>
         <p>Это самая последняя версия, включающая в себя все новые и передовые технологии работы с
           компьютером.</p>
-        <p>Для скачивания Ubuntu 20.04.3 через торрент или напрямую можно кликнуть по нужной ссылке из таблицы
+        <p>Для скачивания Ubuntu 22.04 через торрент или напрямую можно кликнуть по нужной ссылке из таблицы
           ниже.</p>
         <table className="table  table-bordered">
           <tbody>
@@ -54,37 +53,33 @@ export default function DownloadPage({topPosts}: ILayoutProps) {
             <td rowSpan={1}>Мир</td>
             <td className="ltd">Десктоп</td>
 
-            <td className="ltd"><a href="http://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso"
+            <td className="ltd"><a href="https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso"
                                    rel="nofollow">amd64</a></td>
 
-            <td className="ltd"><a href="http://releases.ubuntu.com/20.04/ubuntu-20.04.3-desktop-amd64.iso.torrent"
+            <td className="ltd"><a href="https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso.torrent"
                                    rel="nofollow">amd64</a></td>
           </tr>
           <tr>
             <td rowSpan={1}>Россия</td>
             <td>Десктоп</td>
 
-            <td><a href="http://mirror.yandex.ru/ubuntu-releases/20.04/ubuntu-20.04.3-desktop-amd64.iso"
+            <td><a href="https://mirror.yandex.ru/ubuntu-releases/22.04/ubuntu-22.04-desktop-amd64.iso"
                    rel="nofollow">amd64</a></td>
 
-            <td><a href="http://mirror.yandex.ru/ubuntu-releases/20.04/ubuntu-20.04.3-desktop-amd64.iso.torrent"
+            <td><a href="https://mirror.yandex.ru/ubuntu-releases/22.04/ubuntu-22.04-desktop-amd64.iso.torrent"
                    rel="nofollow">amd64</a></td>
           </tr>
           </tbody>
         </table>
 
-
         <h2>Промежуточные</h2>
-        <p>Выходят каждые полгода и несут в себе новые и экспериментальные функции,
-            так что если хотите идти в ногу со временем, этот выпуск будет актуален.
+        <p>Выходят каждые полгода и несут в себе новые и экспериментальные функции, так что если хотите идти в ногу со временем, этот выпуск будет актуален.
           </p>
 
-        <p>Поддержка их кратковременна,
-          всего три месяца после выхода новой версии, так что если выбрали этот путь, то придётся постоянно обновляться.</p>
+        <p>Поддержка их кратковременна, всего 9 месяцев после релиза, поэтому придётся постоянно обновляться.</p>
 
         <h2 id="21.10">Скачать Ubuntu 21.10</h2>
-        <p>Это последняя промежуточная версия, включающая в себя все новые и передовые технологии работы с
-          компьютером.</p>
+
         <p>Для скачивания через торрент или напрямую можно кликнуть по нужной ссылке из таблицы ниже.</p>
         <table className="table  table-bordered">
           <tbody>
@@ -134,10 +129,10 @@ export default function DownloadPage({topPosts}: ILayoutProps) {
           в том числе и Ubuntu, продолжают использовать эту исторически сложившуюся терминологию.</p>
         <p>Стоит отметить такой важный момент: i386 поддерживает не более 4 ГБ оперативной памяти, а amd64 — 16ТБ.</p>
 
-        <p>К радости, использующих старое оборудование, убунту всё ещё поддерживает 32-х разрядные системы.
-          Этим дистрибутивом является 16.04. Чья поддержка всё ещё актуальна до 2024 года.</p>
+        <p>К радости, использующих старое оборудование, Убунту всё ещё поддерживает 32-х разрядные системы.
+          Этим дистрибутивом является 16.04.6. Чья поддержка всё ещё актуальна до 2024 года.</p>
 
-        <h2 id="16.04">Скачать Ubuntu 16.04.7 LTS</h2>
+        <h2 id="16.04">Скачать Ubuntu 16.04.7 и 16.04.6 (для 32-х)</h2>
         <p>Это предпоследняя стабильная версия, которая будет обновляться до апреля 2021г. Текущая версия последний
           дистрибутив Ubuntu с десктоп оболочкой Unity. Следующий релиз идёт уже с переделанным GNOME Shell.</p>
         <p>Для скачивания Ubuntu 16.04.7 LTS через торрент или напрямую можно кликнуть по нужной ссылке из таблицы
@@ -158,25 +153,25 @@ export default function DownloadPage({topPosts}: ILayoutProps) {
           <tr>
             <td rowSpan={1}>Мир</td>
             <td className="ltd">Десктоп</td>
-            <td className="ltd"><a href="http://releases.ubuntu.com/16.04/ubuntu-16.04.6-desktop-i386.iso"
+            <td className="ltd"><a href="https://releases.ubuntu.com/16.04/ubuntu-16.04.6-desktop-i386.iso"
                                    rel="nofollow">i386</a></td>
-            <td className="ltd"><a href="http://releases.ubuntu.com/16.04/ubuntu-16.04.7-desktop-amd64.iso"
+            <td className="ltd"><a href="https://releases.ubuntu.com/16.04/ubuntu-16.04.7-desktop-amd64.iso"
                                    rel="nofollow">amd64</a></td>
-            <td className="ltd"><a href="http://releases.ubuntu.com/16.04/ubuntu-16.04.6-desktop-i386.iso.torrent"
+            <td className="ltd"><a href="https://releases.ubuntu.com/16.04/ubuntu-16.04.6-desktop-i386.iso.torrent"
                                    rel="nofollow">i386</a></td>
-            <td className="ltd"><a href="http://releases.ubuntu.com/16.04/ubuntu-16.04.7-desktop-amd64.iso.torrent"
+            <td className="ltd"><a href="https://releases.ubuntu.com/16.04/ubuntu-16.04.7-desktop-amd64.iso.torrent"
                                    rel="nofollow">amd64</a></td>
           </tr>
           <tr>
             <td rowSpan={1}>Россия</td>
             <td>Десктоп</td>
-            <td><a href="http://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.6-desktop-i386.iso"
+            <td><a href="https://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.6-desktop-i386.iso"
                    rel="nofollow">i386</a></td>
-            <td><a href="http://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.7-desktop-amd64.iso"
+            <td><a href="https://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.7-desktop-amd64.iso"
                    rel="nofollow">amd64</a></td>
-            <td><a href="http://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.6-desktop-i386.iso.torrent"
+            <td><a href="https://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.6-desktop-i386.iso.torrent"
                    rel="nofollow">i386</a></td>
-            <td><a href="http://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.7-desktop-amd64.iso.torrent"
+            <td><a href="https://mirror.yandex.ru/ubuntu-releases/16.04/ubuntu-16.04.7-desktop-amd64.iso.torrent"
                    rel="nofollow">amd64</a></td>
           </tr>
           </tbody>
